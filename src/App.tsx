d9e14d7afa3d50850f54d766d3c56b36699e5b7f
nick momotenko <nickmomotenkoforweb@gmail.com>
2025-01-18 13:39:09 +0300

    fixed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Router, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import { Container } from "./components/Container";
 import { Menu } from "./components/Menu";
 import { CatsContainer } from "./containers/CatsContainer";
