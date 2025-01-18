088436351f3f25cb8b90fb173f58b1edd7def105
nick momotenko <nickmomotenkoforweb@gmail.com>
2025-01-18 14:18:31 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter
-    basename={
-      window.location.pathname.split("/").slice(1, 2).join("/")
-        ? `/${window.location.pathname.split("/").slice(1, 2).join("/")}`
-        : ""
-    }
-  >
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
