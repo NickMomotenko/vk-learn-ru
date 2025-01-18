27545937f2612e3cbf3bcd78d91440136fdc722b
nick momotenko <nickmomotenkoforweb@gmail.com>
2025-01-18 13:31:24 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Router, Routes } from "react-router";
 import { Container } from "./components/Container";
 import { Menu } from "./components/Menu";
 import { CatsContainer } from "./containers/CatsContainer";
@@ -11,8 +11,8 @@ function App() {
       <Menu />
       <Content>
         <Routes>
-          <Route path="" element={<CatsContainer />} />
-          <Route path="liked" element={<LikedCatsContainer />} />
+          <Route path="/" element={<CatsContainer />} />
+          <Route path="/liked" element={<LikedCatsContainer />} />
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
