e238875731804efe4cbbe65d6e5da3eed1d240fe
nick momotenko <nickmomotenkoforweb@gmail.com>
2025-01-18 14:23:26 +0300

    fixed router

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/vk-learn-ru">
     <App />
   </BrowserRouter>
 );
